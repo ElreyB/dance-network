0c73fdbd55053e56f9e74fd6127455e7093b8658
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-03 09:47:21 -0700

    add company model to app

---
+++ src/app/company.model.ts
@@ -0,0 +1,10 @@
+export class Company {
+  constructor(
+    public companyName: string,
+    public type: string,
+    public choreographer: string,
+    public companyMembers: string,
+    public about: string,
+    public website: string
+  ) {}
+}
